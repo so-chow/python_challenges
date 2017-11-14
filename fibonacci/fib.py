e87250de37c81b638c1bcb8ec3fa351a449228ca
Sophia Chow <Sophia.t.chow@gmail.com>
2017-11-13 19:22:38 -0600

    Soph python hw complete

--- fibonacci/fib.py
+++ fibonacci/fib.py
@@ -1,2 +1,5 @@
 def fib(x):
-  # Your code here
+    if x==1 or x==2:
+        return 1
+    return fib(x-1)+fib(x-2)
+print fib(6)
---
+++ fizzbuzz/fizzbuzz.py
@@ -0,0 +1,12 @@
+def fizzbuzz():
+    for num in range(1,101):
+        if num % 3 == 0 and num % 5 == 0:
+            print("fizzbuzz")
+        elif num % 3 == 0:
+            print("Fizz")
+        elif num % 5 == 0:
+            print("Buzz")
+        else:
+            print(num)
+
+fizzbuzz()
